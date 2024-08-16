23fb2d3ab3ea04e56d10e35968345db086484be9
Tim Guggenmos <tim.guggenmos@me.com>
2024-08-16 18:22:07 +0200

    remove Icon reexport from icons package

--- packages/tui-icons-svelte/src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @file Reexport `Icon` and `IconType` from `svelte-awesome` for easy use
- */
-
-import { default as Icon, IconType } from "svelte-awesome/components/Icon.svelte"
-export { Icon, IconType }
