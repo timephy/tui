87ab5f365a8e3f5a4bc3cc8b72b1f773bf92141c
Tim Guggenmos <tim.guggenmos@me.com>
2024-09-03 12:36:03 +0200

    eslint ignore js in dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,7 @@ export default [
     // JavaScript
     {
         files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+        ignores: ["**/.svelte-kit/**/*", "**/dist/**/*", "**/build"],
     },
 
     // TypeScript
