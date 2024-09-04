e9c4ff22e6c858353613f939970da65dc0656e67
Tim Guggenmos <tim.guggenmos@me.com>
2024-09-04 15:45:09 +0200

    test storage Field page

---
+++ packages/tui-components-svelte/src/routes/storage/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Field } from "$lib/storage/index.svelte"
+    import Switch from "$lib/ui/Switch.svelte"
+    import { onMount } from "svelte"
+
+    const field = new Field("page", false, Field.parse_boolean)
+
+    $inspect(field.value)
+
+    let loading = $state(false)
+    onMount(() => {
+        loading = false
+    })
+</script>
+
+{#if loading}
+    Loading...
+{:else}
+    <Switch bind:value={field.value} />
+{/if}
